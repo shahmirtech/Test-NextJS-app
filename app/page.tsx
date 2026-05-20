7bfb560d71570ee9a0c1e9123290dc9dde9af7df
Shahmir <razashahmir007@gmail.com>
2026-05-20 02:27:39 -0700

    Working with Images in next.js

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,7 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
-          className="dark:invert"
-          src="/next.svg"
+          src="https://wallpapers-clan.com/wp-content/uploads/2026/02/lofi-datsun-240z-palm-trees-retro-sunset-desktop-wallpaper-cover.jpg"
           alt="Next.js logo"
           width={100}
           height={20}
